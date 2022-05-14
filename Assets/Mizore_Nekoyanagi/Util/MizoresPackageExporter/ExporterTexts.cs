89eb14c2e037b5231b0473424f3a846c8bb0fde5
SleetCat123 <37854026+SleetCat123@users.noreply.github.com>
2022-05-15 00:50:13 +0900

    英語版と日本語版テキストを別の定数にしてみたけど、そもそもこういうテキストってjsonとかで記述したほうがいいのでは感があるので一旦放置

--- Assets/Mizore_Nekoyanagi/Util/MizoresPackageExporter/ExporterTexts.cs
+++ Assets/Mizore_Nekoyanagi/Util/MizoresPackageExporter/ExporterTexts.cs
@@ -15,13 +15,18 @@ namespace MizoreNekoyanagi.PublishUtil.PackageExporter
         public const string TEXT_BUTTON_EXPORT_M = "Export to unitypackages";
         public const string TEXT_BUTTON_OPEN = "Open";
         public const string TEXT_DIFF_LABEL = "?";
-        public const string TEXT_DIFF_TOOLTIP = "Some values are different.\n一部のオブジェクトの値が異なっています。";
+        public const string EN_TEXT_DIFF_TOOLTIP = "Some values are different.";
+        public const string JP_TEXT_DIFF_TOOLTIP = "一部のオブジェクトの値が異なっています。";
         public const string TEXT_BUTTON_FOLDER = "Folder";
         public const string TEXT_BUTTON_FILE = "File";
-        public const string TEXT_EXPORT_LOG_NOT_FOUND = "[{0}] is not exists.\n[{0}]は存在しません。\n";
-        public const string TEXT_EXPORT_LOG_FAILED = "Export has been cancelled.\nエクスポートは中断されました。\n";
-        public const string TEXT_EXPORT_LOG_ALL_FILE_EXISTS = "All files or directories exist.\n全てのファイル／フォルダが存在しています。\n";
-        public const string TEXT_EXPORT_LOG_SUCCESS = "[{0}] Export completed.\n[{0}]のエクスポートに成功しました。\n";
+        public const string EN_TEXT_EXPORT_LOG_NOT_FOUND = "[{0}] is not exists.\n";
+        public const string JP_TEXT_EXPORT_LOG_NOT_FOUND = "[{0}]は存在しません。\n";
+        public const string EN_TEXT_EXPORT_LOG_FAILED = "Export has been cancelled.\n";
+        public const string JP_TEXT_EXPORT_LOG_FAILED = "エクスポートは中断されました。\n";
+        public const string EN_TEXT_EXPORT_LOG_ALL_FILE_EXISTS = "All files or directories exist.\n";
+        public const string JP_TEXT_EXPORT_LOG_ALL_FILE_EXISTS = "ファイル／フォルダの存在チェックが完了しました。\n";
+        public const string EN_TEXT_EXPORT_LOG_SUCCESS = "[{0}] Export completed.\n";
+        public const string JP_TEXT_EXPORT_LOG_SUCCESS = "[{0}]のエクスポートに成功しました。\n";
 
         public static string t_Undo => TEXT_UNDO;
         public static string t_Objects => TEXT_OBJECTS;
@@ -33,12 +38,12 @@ namespace MizoreNekoyanagi.PublishUtil.PackageExporter
         public static string t_Button_ExportPackages => TEXT_BUTTON_EXPORT_M;
         public static string t_Button_Open => TEXT_BUTTON_OPEN;
         public static string t_Diff_Label => TEXT_DIFF_LABEL;
-        public static string t_Diff_Tooltip => TEXT_DIFF_TOOLTIP;
+        public static string t_Diff_Tooltip => EN_TEXT_DIFF_TOOLTIP + JP_TEXT_DIFF_TOOLTIP;
         public static string t_Button_Folder => TEXT_BUTTON_FOLDER;
         public static string t_Button_File => TEXT_BUTTON_FILE;
-        public static string t_ExportLog_NotFound => TEXT_EXPORT_LOG_NOT_FOUND;
-        public static string t_ExportLog_Failed => TEXT_EXPORT_LOG_FAILED;
-        public static string t_ExportLog_AllFileExists => TEXT_EXPORT_LOG_ALL_FILE_EXISTS;
-        public static string t_ExportLog_Success => TEXT_EXPORT_LOG_SUCCESS;
+        public static string t_ExportLog_NotFound => EN_TEXT_EXPORT_LOG_NOT_FOUND + JP_TEXT_EXPORT_LOG_NOT_FOUND;
+        public static string t_ExportLog_Failed => EN_TEXT_EXPORT_LOG_FAILED + JP_TEXT_EXPORT_LOG_FAILED;
+        public static string t_ExportLog_AllFileExists => EN_TEXT_EXPORT_LOG_ALL_FILE_EXISTS + JP_TEXT_EXPORT_LOG_ALL_FILE_EXISTS;
+        public static string t_ExportLog_Success => EN_TEXT_EXPORT_LOG_SUCCESS + JP_TEXT_EXPORT_LOG_SUCCESS;
     }
 }
