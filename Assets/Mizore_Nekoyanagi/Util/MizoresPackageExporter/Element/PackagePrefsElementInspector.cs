d8a1cbc8823bc77114022c5e76240cc4ab2da611
SleetCat123 <37854026+SleetCat123@users.noreply.github.com>
2023-03-31 19:03:50 +0900

    HorizontalScope追加

--- Assets/Mizore_Nekoyanagi/Util/MizoresPackageExporter/Element/PackagePrefsElementInspector.cs
+++ Assets/Mizore_Nekoyanagi/Util/MizoresPackageExporter/Element/PackagePrefsElementInspector.cs
@@ -12,27 +12,29 @@ namespace MizoreNekoyanagi.PublishUtil.PackageExporter
     {
         public static void Draw<T>( PackagePrefsElement element ) where T : UnityEngine.Object {
             bool changed = false;
-            EditorGUI.BeginChangeCheck( );
-            element.Object = EditorGUILayout.ObjectField( element.Object, typeof( T ), false );
-            if ( EditorGUI.EndChangeCheck( ) ) {
-                changed = true;
-            }
-
-            EditorGUI.BeginChangeCheck( );
-            Rect textrect = EditorGUILayout.GetControlRect( );
-            string path = EditorGUI.TextField( textrect, element.Path );
-            if ( ExporterUtils.DragDrop( textrect, ExporterUtils.Filter_HasPersistentObject ) ) {
-                GUI.changed = true;
-                path = AssetDatabase.GetAssetPath( DragAndDrop.objectReferences[0] );
-            }
-
-            if ( EditorGUI.EndChangeCheck( ) ) {
-                // パスが変更されたらオブジェクトを置き換える
-                Object o = AssetDatabase.LoadAssetAtPath<T>( path );
-                if ( o != null ) {
-                    element.Object = o;
+            using ( new EditorGUILayout.HorizontalScope( ) ) {
+                EditorGUI.BeginChangeCheck( );
+                element.Object = EditorGUILayout.ObjectField( element.Object, typeof( T ), false );
+                if ( EditorGUI.EndChangeCheck( ) ) {
+                    changed = true;
                 }
-                changed = true;
+
+                EditorGUI.BeginChangeCheck( );
+                Rect textrect = EditorGUILayout.GetControlRect( );
+                string path = EditorGUI.TextField( textrect, element.Path );
+                if ( ExporterUtils.DragDrop( textrect, ExporterUtils.Filter_HasPersistentObject ) ) {
+                    GUI.changed = true;
+                    path = AssetDatabase.GetAssetPath( DragAndDrop.objectReferences[0] );
+                }
+
+                if ( EditorGUI.EndChangeCheck( ) ) {
+                    // パスが変更されたらオブジェクトを置き換える
+                    Object o = AssetDatabase.LoadAssetAtPath<T>( path );
+                    if ( o != null ) {
+                        element.Object = o;
+                    }
+                    changed = true;
+                }
             }
             GUI.changed = changed;
         }
