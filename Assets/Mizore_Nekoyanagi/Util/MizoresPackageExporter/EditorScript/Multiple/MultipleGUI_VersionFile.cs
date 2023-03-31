40f9681a85dcfd703dbf74e8d127cc2ec15af1a2
SleetCat123 <37854026+SleetCat123@users.noreply.github.com>
2023-04-01 07:22:14 +0900

    複数編集エディターでversionSourceの値が統一されていない場合にバージョン詳細設定を表示しないように

--- Assets/Mizore_Nekoyanagi/Util/MizoresPackageExporter/EditorScript/Multiple/MultipleGUI_VersionFile.cs
+++ Assets/Mizore_Nekoyanagi/Util/MizoresPackageExporter/EditorScript/Multiple/MultipleGUI_VersionFile.cs
@@ -14,13 +14,13 @@ namespace MizoreNekoyanagi.PublishUtil.PackageExporter.MultipleEditor
         public static void Draw( MizoresPackageExporter t, IEnumerable<MizoresPackageExporter> targetlist ) {
             if ( ExporterUtils.EditorPrefFoldout(
                 Const.EDITOR_PREF_FOLDOUT_VERSIONFILE, ExporterTexts.t_Version ) ) {
+                var same_versionSource_valueInAllObj = targetlist.All( v => t.versionSource == v.versionSource );
                 using ( new EditorGUILayout.HorizontalScope( ) ) {
                     ExporterUtils.Indent( 1 );
-                    var samevalue_in_all_obj = targetlist.All( v => t.versionSource == v.versionSource );
                     EditorGUI.BeginChangeCheck( );
                     MizoresPackageExporter.VersionSource versionSource;
 
-                    EditorGUI.showMixedValue = !samevalue_in_all_obj;
+                    EditorGUI.showMixedValue = !same_versionSource_valueInAllObj;
                     versionSource = (MizoresPackageExporter.VersionSource)EditorGUILayout.EnumPopup( ExporterTexts.t_VersionSource, t.versionSource );
                     EditorGUI.showMixedValue = false;
 
@@ -32,69 +32,48 @@ namespace MizoreNekoyanagi.PublishUtil.PackageExporter.MultipleEditor
                         }
                     }
                 }
-                using ( new EditorGUILayout.HorizontalScope( ) ) {
-                    ExporterUtils.Indent( 1 );
-                    switch ( t.versionSource ) {
-                        case MizoresPackageExporter.VersionSource.String: {
-                            var samevalue_in_all_obj = targetlist.All( v => t.versionString == v.versionString );
-                            EditorGUI.BeginChangeCheck( );
-                            string versionString;
+                if ( same_versionSource_valueInAllObj ) {
+                    using ( new EditorGUILayout.HorizontalScope( ) ) {
+                        ExporterUtils.Indent( 1 );
+                        switch ( t.versionSource ) {
+                            case MizoresPackageExporter.VersionSource.String: {
+                                var samevalue_in_all_obj = targetlist.All( v => t.versionString == v.versionString );
+                                EditorGUI.BeginChangeCheck( );
+                                string versionString;
 
-                            EditorGUI.showMixedValue = !samevalue_in_all_obj;
-                            versionString = EditorGUILayout.TextField( ExporterTexts.t_Version, t.versionString );
-                            EditorGUI.showMixedValue = false;
-
-                            if ( EditorGUI.EndChangeCheck( ) ) {
-                                foreach ( var item in targetlist ) {
-                                    item.versionString = versionString;
-                                    item.UpdateExportVersion( );
-                                    EditorUtility.SetDirty( item );
-                                }
-                            }
-                            break;
-                        }
-                        case MizoresPackageExporter.VersionSource.File: {
-                            var samevalue_in_all_obj = targetlist.All( v => t.versionFile.Object == v.versionFile.Object );
-
-                            EditorGUI.BeginChangeCheck( );
-                            Object obj;
-                            if ( samevalue_in_all_obj ) {
-                                obj = EditorGUILayout.ObjectField( t.versionFile.Object, typeof( TextAsset ), false );
-                            } else {
-                                ExporterUtils.DiffLabel( );
-                                EditorGUI.showMixedValue = true;
-                                obj = EditorGUILayout.ObjectField( null, typeof( TextAsset ), false );
+                                EditorGUI.showMixedValue = !samevalue_in_all_obj;
+                                versionString = EditorGUILayout.TextField( ExporterTexts.t_Version, t.versionString );
                                 EditorGUI.showMixedValue = false;
-                            }
-                            if ( EditorGUI.EndChangeCheck( ) ) {
-                                foreach ( var item in targetlist ) {
-                                    item.versionFile.Object = obj;
-                                    item.UpdateExportVersion( );
-                                    EditorUtility.SetDirty( item );
-                                }
-                            }
 
-                            EditorGUI.BeginChangeCheck( );
-                            string path;
-                            if ( samevalue_in_all_obj ) {
-                                path = EditorGUILayout.TextField( t.versionFile.Path );
-                            } else {
-                                EditorGUI.showMixedValue = true;
-                                path = EditorGUILayout.TextField( string.Empty );
-                                EditorGUI.showMixedValue = false;
-                            }
-                            if ( EditorGUI.EndChangeCheck( ) ) {
-                                // パスが変更されたらオブジェクトを置き換える
-                                Object o = AssetDatabase.LoadAssetAtPath<TextAsset>( path );
-                                if ( o != null ) {
+                                if ( EditorGUI.EndChangeCheck( ) ) {
                                     foreach ( var item in targetlist ) {
-                                        item.versionFile.Object = o;
+                                        item.versionString = versionString;
                                         item.UpdateExportVersion( );
                                         EditorUtility.SetDirty( item );
                                     }
                                 }
+                                break;
                             }
-                            break;
+                            case MizoresPackageExporter.VersionSource.File: {
+                                var samevalue_in_all_obj = targetlist.All( v => t.versionFile.Object == v.versionFile.Object );
+
+                                if ( !samevalue_in_all_obj ) {
+                                    ExporterUtils.DiffLabel( );
+                                }
+                                EditorGUI.showMixedValue = !samevalue_in_all_obj;
+                                EditorGUI.BeginChangeCheck( );
+                                PackagePrefsElementInspector.Draw<TextAsset>( t.versionFile );
+                                EditorGUI.showMixedValue = false;
+                                if ( EditorGUI.EndChangeCheck( ) ) {
+                                    var obj = t.versionFile.Object;
+                                    foreach ( var item in targetlist ) {
+                                        item.versionFile.Object = obj;
+                                        item.UpdateExportVersion( );
+                                        EditorUtility.SetDirty( item );
+                                    }
+                                }
+                                break;
+                            }
                         }
                     }
                 }
@@ -104,14 +83,12 @@ namespace MizoreNekoyanagi.PublishUtil.PackageExporter.MultipleEditor
                     var samevalue_in_all = targetlist.All( v => t.versionFormat == v.versionFormat );
                     EditorGUI.BeginChangeCheck( );
                     string value;
-                    if ( samevalue_in_all ) {
-                        value = EditorGUILayout.TextField( ExporterTexts.t_VersionFormat, t.versionFormat );
-                    } else {
+                    if ( !samevalue_in_all ) {
                         ExporterUtils.DiffLabel( );
                         EditorGUI.showMixedValue = true;
-                        value = EditorGUILayout.TextField( ExporterTexts.t_VersionFormat, string.Empty );
-                        EditorGUI.showMixedValue = false;
                     }
+                    value = EditorGUILayout.TextField( ExporterTexts.t_VersionFormat, t.versionFormat );
+                    EditorGUI.showMixedValue = false;
                     if ( EditorGUI.EndChangeCheck( ) ) {
                         foreach ( var item in targetlist ) {
                             item.versionFormat = value;
