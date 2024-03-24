2c9c1c671b4d6491997e7dfe8dcd3c8956b0612c
SleetCat123 <37854026+SleetCat123@users.noreply.github.com>
2024-03-24 12:37:32 +0900

    Updater実装注

---
+++ Packages/com.mizore-nekoyanagi.utils.mizore-package-exporter/PackageExporter/Object/MizoresPackageExporterUpdator.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Linq;
+using MizoreNekoyanagi.PublishUtil.PackageExporterV1;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MizoreNekoyanagi.PublishUtil.PackageExporter {
+    public static class MizoresPackageExporterUpdator {
+        public static bool ConvertToLatest( ScriptableObject obj ) {
+            bool converted = false;
+#pragma warning disable 612
+            var v1 = obj as MizoresPackageExporterV1;
+            // 初回実行時
+            if ( v1 != null && v1.packageExporterVersion == MizoresPackageExporter.INITIAL_PACKAGE_EXPORTER_OBJECT_VERSION ) {
+                ExporterUtils.DebugLog( "Initialize" );
+                // packageExporterVersion実装前のオブジェクトからの変換
+                if ( v1.versionFile != null && !v1.versionFile.IsEmpty( ) ) {
+                    // versionFileの場所変更
+                    ExporterUtils.DebugLog( "Convert: versionFile" );
+                    v1.packageNameSettings.versionSource = MizoresPackageExporterV1.VersionSource.File;
+                    v1.packageNameSettings.versionFile = v1.versionFile;
+                    v1.versionFile = null;
+                }
+                if ( !string.IsNullOrEmpty( v1.versionFormat ) ) {
+                    // versionFormatの場所変更
+                    ExporterUtils.DebugLog( "Convert: versionFormat" );
+                    v1.packageNameSettings.versionFormat = v1.versionFormat;
+                    v1.versionFormat = null;
+                }
+                if ( !string.IsNullOrEmpty( v1.packageName ) ) {
+                    // packageNameの場所変更
+                    ExporterUtils.DebugLog( "Convert: packageName" );
+                    v1.packageNameSettings.packageName = v1.packageName;
+                    v1.packageName = null;
+                }
+                converted = true;
+            }
+            if ( packageExporterVersion < 2 ) {
+                ExporterUtils.DebugLog( "Convert: references" );
+                // referencesの場所変更
+                references2 = references.Select( v => new ReferenceElement( v, ReferenceMode.Include ) ).ToList( );
+                references.Clear( );
+
+                // dynamicpathの場所変更
+                dynamicpath2 = dynamicpath.Select( v => new DynamicPathElement( v ) ).ToList( );
+                dynamicpath.Clear( );
+
+                converted = true;
+            }
+            if ( converted ) {
+                Debug.Log( $"Convert version: {packageExporterVersion} -> {CURRENT_PACKAGE_EXPORTER_OBJECT_VERSION}" );
+                packageExporterVersion = CURRENT_PACKAGE_EXPORTER_OBJECT_VERSION;
+            }
+            return converted;
+#pragma warning restore 612
+        }
+    }
+}
---
+++ Packages/com.mizore-nekoyanagi.utils.mizore-package-exporter/PackageExporter/Object/MizoresPackageExporterV1.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using ExporterUtils = MizoreNekoyanagi.PublishUtil.PackageExporter.ExporterUtils;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MizoreNekoyanagi.PublishUtil.PackageExporterV1 {
+    [Obsolete]
+    public class MizoresPackageExporterV1 : ScriptableObject, ISerializationCallbackReceiver {
+        public const int CURRENT_PACKAGE_EXPORTER_OBJECT_VERSION = 1;
+
+        [System.Serializable]
+        private class PackageNameSettingsKVP {
+            public string key;
+            public PackageNameSettings value;
+
+            public PackageNameSettingsKVP( string key, PackageNameSettings value ) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        [SerializeField]
+        public int packageExporterVersion = INITIAL_PACKAGE_EXPORTER_OBJECT_VERSION;
+
+        public List<PackagePrefsElement> objects = new List<PackagePrefsElement>( );
+        public List<string> dynamicpath = new List<string>( );
+        [SerializeField]
+        DynamicPathVariable[] s_variables;
+        [System.NonSerialized]
+        public Dictionary<string, string> variables = new Dictionary<string, string>( );
+
+        public List<PackagePrefsElement> excludeObjects = new List<PackagePrefsElement>( );
+        public List<SearchPath> excludes = new List<SearchPath>( );
+        public List<PackagePrefsElement> references = new List<PackagePrefsElement>( );
+
+        /// <summary>互換性のため残しておく。今後はpackageNameSettings.versionFileを使用</summary>
+        [System.Obsolete, SerializeField]
+        public PackagePrefsElement versionFile;
+
+        /// <summary>互換性のため残しておく。今後はpackageNameSettings.versionFormatを使用</summary>
+        [System.Obsolete, SerializeField]
+        public string versionFormat = null;
+
+        /// <summary>互換性のため残しておく。今後はpackageNameSettings.packageNameを使用</summary>
+        [System.Obsolete, SerializeField]
+        public string packageName = null;
+
+        public PackageNameSettings packageNameSettings = new PackageNameSettings( );
+
+        [SerializeField]
+        PackageNameSettingsKVP[] s_packageNameSettingsOverride;
+        [System.NonSerialized]
+        public Dictionary<string, PackageNameSettings> packageNameSettingsOverride = new Dictionary<string, PackageNameSettings>( );
+
+        #region BatchExport
+        public BatchExportMode batchExportMode;
+        public BatchExportFolderMode batchExportFolderMode;
+        public List<string> batchExportTexts = new List<string>();
+        public PackagePrefsElement batchExportFolderRoot;
+        public PackagePrefsElement batchExportListFile;
+        public string batchExportFolderRegex;
+        #endregion
+
+        public class FilePathList {
+            public IEnumerable<string> paths;
+            public IEnumerable<string> excludePaths;
+            public Dictionary<string, HashSet<string>> referencedPaths;
+        }
+
+        public void OnBeforeSerialize( ) {
+            s_variables = variables.Select( kvp => new DynamicPathVariable( kvp.Key, kvp.Value ) ).ToArray( );
+            s_packageNameSettingsOverride = packageNameSettingsOverride.Select( kvp => new PackageNameSettingsKVP( kvp.Key, kvp.Value ) ).ToArray( );
+        }
+
+        public void OnAfterDeserialize( ) {
+            if ( s_variables != null ) {
+                variables = s_variables.ToDictionary( v => v.key, v => v.value );
+            }
+            if ( s_packageNameSettingsOverride != null ) {
+                packageNameSettingsOverride = s_packageNameSettingsOverride.ToDictionary( v => v.key, v => v.value );
+            }
+        }
+
+        public enum BatchExportFolderMode {
+            All, Files, Folders
+        }
+        public enum BatchExportMode {
+            Single, Texts, Folders, ListFile
+        }
+        [System.Serializable]
+        public class PackagePrefsElement {
+            [SerializeField]
+            public UnityEngine.Object obj;
+            [SerializeField]
+            public string path;
+            public bool IsEmpty( ) {
+                return obj == null && string.IsNullOrEmpty( path );
+            }
+        }
+        [System.Serializable]
+        public class DynamicPathVariable {
+            public string key, value;
+
+            public DynamicPathVariable( string key, string value ) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+        public enum VersionSource {
+            String, File
+        }
+        [System.Serializable]
+        public class PackageNameSettings {
+            /// <summary>jsonのdeserialize用</summary>
+            [System.Serializable]
+            private class VersionJson {
+                public string version;
+            }
+
+            public VersionSource versionSource;
+            public PackagePrefsElement versionFile;
+            public string versionString;
+            public string versionFormat;
+            public string batchFormat;
+            public string packageName;
+
+            public bool useOverride_version = true;
+            public bool useOverride_versionFormat;
+            public bool useOverride_batchFormat;
+            public bool useOverride_packageName;
+        }
+        [System.Serializable]
+        public class SearchPath : ISerializationCallbackReceiver {
+            // 互換性のためSerialize対象にしておく
+            public SearchPathType searchType;
+            [SerializeField]string s_searchType;
+            public string value;
+            public void OnBeforeSerialize( ) {
+                s_searchType = SearchPathTypeExtensions.GetString( searchType );
+            }
+
+            public void OnAfterDeserialize( ) {
+                if ( string.IsNullOrEmpty( s_searchType ) ) {
+                    // enumのstring保存が未実装なデータを読み込んだ場合のみ発生
+                    // enumの順番が変わった場合はここで対応する
+                } else {
+                    searchType = SearchPathTypeExtensions.Parse( s_searchType );
+                }
+            }
+            public enum SearchPathType {
+                Disabled,
+                /// <summary>
+                /// 完全一致
+                /// </summary>
+                Exact,
+                /// <summary>
+                /// 部分一致
+                /// </summary>
+                Partial,
+                /// <summary>
+                /// 部分一致（大文字小文字を無視）
+                /// </summary>
+                Partial_IgnoreCase,
+                /// <summary>
+                /// 正規表現
+                /// </summary>
+                Regex,
+                /// <summary>
+                /// 正規表現（大文字小文字を無視）
+                /// </summary>
+                Regex_IgnoreCase,
+            }
+            public static class SearchPathTypeExtensions {
+                public static string GetString( SearchPathType value ) {
+                    switch ( value ) {
+                        case SearchPathType.Disabled: return "Disabled";
+                        case SearchPathType.Exact: return "Exact";
+                        case SearchPathType.Partial: return "Partial";
+                        case SearchPathType.Partial_IgnoreCase: return "Partial_IgnoreCase";
+                        case SearchPathType.Regex: return "Regex";
+                        case SearchPathType.Regex_IgnoreCase: return "Regex_IgnoreCase";
+                        default: throw new System.ArgumentException( );
+                    }
+                }
+                public static SearchPathType Parse( string value ) {
+                    switch ( value ) {
+                        case "Disabled": return SearchPathType.Disabled;
+                        case "Exact": return SearchPathType.Exact;
+                        case "Partial": return SearchPathType.Partial;
+                        case "Partial_IgnoreCase": return SearchPathType.Partial_IgnoreCase;
+                        case "Regex": return SearchPathType.Regex;
+                        case "Regex_IgnoreCase": return SearchPathType.Regex_IgnoreCase;
+                        default: throw new System.ArgumentException( );
+                    }
+                }
+            }
+        }
+    }
+}
